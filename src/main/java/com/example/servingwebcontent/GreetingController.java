6f149e1a628442a8b55ee8d32fd665426971d633
Tavorot <kubatan77@yandex.ru>
2020-01-08 22:51:40 +0500

    add new files

---
+++ src/main/java/com/example/servingwebcontent/GreetingController.java
@@ -0,0 +1,23 @@
+package com.example.servingwebcontent;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name = "name", required = false, defaultValue = " Не знаю") String name, Model model)
+    {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+    @GetMapping
+    public String lol(@RequestParam(name = "name", required = false, defaultValue = " ") String name, Model model)
+    {
+        model.addAttribute("name", name);
+       return "lol";
+    }
+}
